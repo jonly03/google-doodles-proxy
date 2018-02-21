5ff99986cd4b11a9c3688afad1a0470d2b1a449d
Nelly Sugu <nelly.sugu@gmail.com>
2018-02-21 06:19:34 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+// Simple Express Proxy Server to make request to google/doodles api
+// This request in the browser causes Cross Origin Resource Sharing (CORS) error
+// Hence the need to have a proxy server that directly fetches that information from google servers
+let express = require('express');
+let fetch = require('node-fetch');
+
+let app = express();
+let port = process.env.PORT || 8080;
+let ip = process.env.IP
+
+// Enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get("/doodles/:year/:month", function(req, res){
+  if (!req.params.year || !req.params.month){
+    return res.send({status:400, msg:'Incomplete request /doodles/:year/:month expects both year and month'});
+  }
+  
+  fetch(`https://www.google.com/doodles/json/${req.params.year}/${req.params.month}`)
+    .then(response => response.json())
+    .then(json => {
+      return res.send(json);
+    })
+  
+})
+
+app.listen(port, ip);
