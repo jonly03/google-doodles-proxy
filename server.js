a2d7af659fc1e597a81c2e8fb02abaf43ef7fab8
SiriusScaper <siriusscaper@gmail.com>
2018-06-30 16:23:29 -0700

    Updated country params with working destructured syntax.

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(function (req, res, next) {
 });
 
 
-app.get("/doodles/date/:year/:month", function (req, res) {
+app.get("/doodles/:year/:month", function (req, res) {
   if (!Number(req.params.year) || !Number(req.params.month)) {
     return res.send({ status: 400, msg: 'Bad request /doodles/:year/:month expects a valid year and month' });
   }
@@ -29,45 +29,46 @@ app.get("/doodles/date/:year/:month", function (req, res) {
 
 })
 
+
 app.get('/doodles/country/:country/date/:year/:month/:day', (req, res) => {
   if (!Number(req.params.year) || !Number(req.params.month) && !Number(req.params.day)) {
     return res.send({ status: 400, msg: 'Bad request /doodles/country/:country/date/:year/:month expects a valid year and month' });
   }
-  //TODO: Validate country
-  const country = req.params.country;
+
+  // TODO: Validate country
+  const { country } = req.params;
   fetch(`https://www.google.com/doodles/search?q=${country}&s=0`)
     .then(response => response.json())
-    .then(json => {
+    .then((json) => {
       const { doodles } = json; // const doodles = json.doodles
 
-      const doodlesOfTheDay = doodles.filter((doodle) => {
-        return doodle.run_date_array[0] === Number(req.params.year) && doodle.run_date_array[1] === Number(req.params.month) && doodle.run_date_array[2] === Number(req.params.day);
-      })
+      const doodlesOfTheDay = doodles.filter(doodle => doodle.run_date_array[0] === Number(req.params.year) && doodle.run_date_array[1] === Number(req.params.month) && doodle.run_date_array[2] === Number(req.params.day));
 
-      const finalDoodles = doodlesOfTheDay.map(doodle => {
-        const { name, title, url, run_date_array } = doodle;
+      const finalDoodles = doodlesOfTheDay.map((doodle) => {
+        const {
+          name, title, url, run_date_array,
+        } = doodle;
         return {
           name,
           title,
           url,
-          run_date_array
-        }
-      })
+          run_date_array,
+        };
+      });
 
       return res.json(finalDoodles);
     })
-    .catch(err => res.status(500).send())
-})
+    .catch(err => res.status(500).send());
+});
 
 
 app.get('/doodles/country/:country', (req, res) => {
-  //TODO: Validate country
-  const country = req.params.country;
+  // TODO: Validate country
+  const { country } = req.params;
   fetch(`https://www.google.com/doodles/search?q=${country}&s=0`)
     .then(response => response.json())
-    .then(json => res.send(json))
-})
-
+    .then(json => res.send(json));
+});
 
 app.get("*", function (req, res) {
   return res.send({ status: 404, msg: 'Route does not exist. Only route available is /doodles/:year/:month' });
